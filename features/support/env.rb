b7e6cbb8781250e60b34bdca322b1596d735886e
glaucia <glaucia.admachado@gmail.com>
2018-11-25 13:38:09 -0200

    headless

--- features/support/env.rb
+++ features/support/env.rb
@@ -16,8 +16,21 @@ Capybara.register_driver :selenium do |app|
 if BROWSER.eql?('chrome')
   Capybara::Selenium::Driver.new(app, :browser => :chrome)
 
+elsif BROWSER.eql?('chrome_headless')
+    Capybara::Selenium::Driver.new(app, :browser => :chrome,
+    desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(
+     'chromeOptions' => {'args' =>['--headless', 'disable-gpu']}
+    )
+    )
+
 elsif BROWSER.eql?('firefox')
   Capybara::Selenium::Driver.new(app, :browser => :firefox, :marionette =>true)
+
+elsif BROWSER.eql?('firefox_headless')
+    browser_options = Selenium::WebDriver::Firefox::Options.new(args: ['--headless'])
+    Capybara::Selenium::Driver.new(
+      app, :browser => :firefox,
+      options: browser_options)
 
 #elsif BROWSER.eql?('ie')  
  # Capybara::Selenium::Driver.new(app, :browser => :internet_explorer)
